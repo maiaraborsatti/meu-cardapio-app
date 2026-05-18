27becf3e9e247e641de3b9779ee40a4efac51033
maiaraborsatti <maiara.borsatti@gmail.com>
2026-05-18 18:45:03 -0300

    feat: adicionar tema global e botão customizado

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF8BC34A);
+  static const Color white = Colors.white;
+  static const Color yellow = Color(0xFFFDD835);
+
+  static const Color shadow = Colors.black26;
+}
