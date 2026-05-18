79c03f243be98138002e076e31870f97ca5f04e9
maiaraborsatti <maiara.borsatti@gmail.com>
2026-05-18 18:45:45 -0300

    feat: criar tela de carregamento inicial

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../theme/app_colors.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryGreen,
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png',
+          width: 250,
+        ),
+      ),
+    );
+  }
+}
