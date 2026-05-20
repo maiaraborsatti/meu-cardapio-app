0db3ee9b242cce5e5eea98a26f073ca8150cb7af
maiaraborsatti <maiara.borsatti@gmail.com>
2026-05-19 21:27:55 -0300

    feat: criar card de avatar para seleção de usuários

---
+++ lib/widgets/usuario_avatar_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../models/usuario.dart';
+
+class UsuarioAvatarCard extends StatelessWidget {
+  final Usuario usuario;
+  final VoidCallback onTap;
+
+  const UsuarioAvatarCard({
+    super.key,
+    required this.usuario,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+
+      child: Column(
+        children: [
+
+          Container(
+            width: 90,
+            height: 90,
+
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 3,
+              ),
+            ),
+
+            child: ClipOval(
+              child: usuario.foto != null
+                  ? Image.asset(
+                      usuario.foto!,
+                      fit: BoxFit.cover,
+                    )
+                  : Container(
+                      color: Colors.white,
+                      child: const Icon(
+                        Icons.person,
+                        size: 50,
+                        color: Colors.grey,
+                      ),
+                    ),
+            ),
+          ),
+
+          const SizedBox(height: 8),
+
+          Text(
+            usuario.nome,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
